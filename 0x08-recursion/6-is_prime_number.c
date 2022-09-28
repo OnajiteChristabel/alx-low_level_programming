74c6f9ac2a70e8bf2f4617201b80c0028b9e1c75
Fortunejite <111015282+Fortunejite@users.noreply.github.com>
2022-09-28 21:11:32 +0100

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -25,11 +25,9 @@ int prime(int n, int ans)
 
 int is_prime_number(int n)
 {
-	int ans = 3;
-
 	if (n % 2 == 0 || n < 2)
 		return (0);
 	if (n == 2)
 		return (1);
 
-	return (prime(n, ans));
+	return (prime(n, 3));
