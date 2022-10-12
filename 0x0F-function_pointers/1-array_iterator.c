c0d000d84c6051608393ae35622f11fee74a3b19
Fortunejite <111015282+Fortunejite@users.noreply.github.com>
2022-10-12 13:57:52 +0100

    Update 1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -2,6 +2,12 @@
 #include "function_pointers.h"
 #include <stdlib.h>
 
+/**
+ * array_iterator - execute function on an array via a function ptr
+ * @array: array
+ * @size: size of array
+ * @action: pointer to function that we need to execute
+ */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
