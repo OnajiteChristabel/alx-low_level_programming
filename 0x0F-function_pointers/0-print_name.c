0dcf5f4beac71148b6781cea2baacb1746b43a34
Fortune <fortunejite1234@gmail.com>
2022-10-12 05:22:17 -0700

    m

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,12 @@
 #include "function_pointers.h"
 #include <stdlib.h>
+
 /**
  * print_name - prints name
  * @name: name
  * @f: func
  */
+
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
