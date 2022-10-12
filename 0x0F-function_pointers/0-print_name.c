1e7e696eb2f76fcf442b9942e13506fc26f43438
Fortunejite <111015282+Fortunejite@users.noreply.github.com>
2022-10-12 13:36:01 +0100

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,7 +5,6 @@
  * print_name - execute a function given as a parameter
  * @name: parameter that function ptr needs
  * @f:function to execute
- *
  */
 
 void print_name(char *name, void (*f)(char *))
