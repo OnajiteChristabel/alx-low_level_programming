bbee443942797570e00eb6776f1bce4f0ea0ade5
Fortune <fortunejite1234@gmail.com>
2022-10-12 12:35:57 -0700

    m

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,14 +1,23 @@
 #include "3-calc.h"
 
-int (*get_op_func(char *s))(int, int)
+int (*get_op_func(char *s))(int a, int b)
 {
 	op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
+        {"+", op_add(a,b)},
+        {"-", op_sub(a, b)},
+        {"*", op_mul(a, b)},
+        {"/", op_div(a, b)},
+        {"%", op_mod(a, b)},
         {NULL, NULL}
     };
     int i;
+
+	i = 0;
+	while (ops[i])
+	{
+		if (ops[i].op == s)
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
 }
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,6 @@
+#include "3-calc.h"
+
+int main(int argc, char argv[])
+{
+	get_op_func(char argv[2])(atoi(argv[1]), atoi(argv[3]);
+}
