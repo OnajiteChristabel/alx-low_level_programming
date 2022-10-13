33bb4df5d26c16773a803b03abd6d1b9cd386ddb
Fortunejite <111015282+Fortunejite@users.noreply.github.com>
2022-10-13 15:16:25 +0100

    Update 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,16 +9,13 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned int i;
-	unsigned int sum = 0;
+	unsigned int sum = 0, i;
 	va_list num;
 
-	/* validate valist and initialize */
 	if (n == 0)
 		return (0);
 	va_start(num, n);
 
-	/* iterate through list, update sum, free list */
 	for (i = 0; i < n; i++)
 		sum += va_arg(num, int);
 	va_end(num);
