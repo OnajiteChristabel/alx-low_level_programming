f7cfb8680fde7b584018cfce61a446abcf6a2163
OnajiteChristabel <christabelonajite2005@gmail.com>
2022-10-14 21:26:10 -0700

    j

--- 0x10-variadic_functions/2-main.c
+++ 0x10-variadic_functions/2-main.c
@@ -1,16 +1,23 @@
 #include "variadic_functions.h"
 
+
+
 /**
- * main - check the code
  *
- * Return: Always 0.
- */
+ *  * main - check the code
+ *
+ *   *
+ *
+ *    * Return: Always 0.
+ *
+ *     */
+
 int main(void)
+
 {
-    print_strings(", ", 2, "Jay", "Django");
-    return (0);
-<<<<<<< HEAD
+
+	    print_strings(", ", 2, "Jay", "Django");
+
+	        return (0);
+
 }
-=======
-}
->>>>>>> c813b95e88b1939967b005ac0954cb75787c4356
--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "variadic_functions.h"
 #include <stdio.h>
 #include <stdarg.h>
@@ -28,38 +27,4 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		}
 	printf("\n");
 		va_end(string);
-=======
-#include <stdio.h> /* printf */
-#include "variadic_functions.h"
-
-/**
- * print_strings - print varying amount of strings
- * @separator: delimiter
- * @n: amount of arguments in list
- */
-
-void print_strings(const char *separator, const unsigned int n, ...)
-{
-	va_list valist;
-	unsigned int i;
-	char *s;
-
-	if (n > 0)
-	{
-		va_start(valist, n);
-		for (i = 1; i <= n; i++)
-		{
-			s = va_arg(valist, char *);
-			if (s == NULL)
-				printf("(nil)");
-			else
-				printf("%s", s);
-
-			if (i != n && separator != NULL)
-				printf("%s", separator);
-		}
-		va_end(valist);
-	}
-	printf("\n");
->>>>>>> c813b95e88b1939967b005ac0954cb75787c4356
 }
