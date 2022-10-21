510b120ad2cc7b753830232d18419ca3188fb5ac
OnajiteChristabel <christabelonajite2005@gmail.com>
2022-10-21 13:29:58 -0700

    grit

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,21 +1,25 @@
 #include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
 /**
- * print_list - prints all the elements of a list_t list
+ * print_list - prints the lists
  * @h: singly linked list
- * Return: number of elements in the lists
+ * Return: number of nodes
  */
 size_t print_list(const list_t *h)
 {
-	size_t nelem;
-
-	nelem = 0;
+	size_t nelem = 0;
 
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
+		{
+			printf("[0] (nil)\n");
+		}
 		else
+		{
 			printf("[%d] %s\n", h->len, h->str);
+		}
 		h = h->next;
 		nelem++;
 	}
