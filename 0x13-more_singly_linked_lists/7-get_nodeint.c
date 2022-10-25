17a18411618f6b1df0f4a46e1a0bf72ecb4d2daa
Fortunejite <111015282+Fortunejite@users.noreply.github.com>
2022-10-25 12:31:34 +0100

    Update 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,5 +1,12 @@
 #include "lists.h"
 
+/**
+ * get_nodeint_at_index - node index
+ * @head: node
+ * @index: index
+ * Return: ptr node
+ */
+
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	listint_t *node;
