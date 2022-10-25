979f9c7a71e8ee1fbdd4e402754a7d58d7197eb2
Fortunejite <111015282+Fortunejite@users.noreply.github.com>
2022-10-25 12:53:50 +0100

    Add files via upload

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+int sum_listint(listint_t *head)
+{
+	int i = 0;
+	listint_t *temp;
+
+	if (head == NULL)
+		return (0);
+	while (*temp != NULL)
+	{
+		i += temp->n
+		temp = temp->next;
+	}
+	free(temp);
+	return (i);
+}
