31f83e09a84a61cbf93c5a1a01829e8a0210063e
Fortunejite <111015282+Fortunejite@users.noreply.github.com>
2022-10-27 18:45:45 +0100

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,10 +1,12 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * print_binary - converts unsigned int to binary
  * @n: unsigned int
  * Return: binary
  */
+
 void print_binary(unsigned long int n)
 {
 
@@ -17,17 +19,14 @@ void print_binary(unsigned long int n)
 		n_copy >>= 1;
 	}
 	len -= 1;
-
-	if (len > 0) /* create mask based on length of num */
+	if (len > 0)
 		mask = mask << len;
-
 	while (mask > 0) /* match each rightmost bit to see if 1 or 0 */
 	{
 		if (n & mask)
 			_putchar('1');
 		else
 			_putchar('0');
-
 		mask >>= 1;
 	}
 }
