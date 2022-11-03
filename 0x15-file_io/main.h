6e8bf67b5eb4b62d69a5129cb133505d7af15920
OnajiteChristabel <111015651+OnajiteChristabel@users.noreply.github.com>
2022-11-03 11:37:40 -0700

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,12 +1,3 @@
-
-#ifndef __MAIN_H__
-#define __MAIN_H__
-
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-=======
 #ifndef HEADER_H
 #define HEADER_H
 
@@ -22,7 +13,6 @@
 #include <stdio.h>
 
 /* malloc, free */
->>>>>>> 0c5e210b372a7aa5b1cda4cd853b06e8052f2679
 #include <stdlib.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
